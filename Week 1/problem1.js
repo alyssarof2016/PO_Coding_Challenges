59e2d6751d0191ebc164c2f006aaf68e158dad18
alyssarof2016 <68204695+alyssarof2016@users.noreply.github.com>
2021-08-02 14:16:29 -0400

    finished problem 1

---
+++ Week 1/problem1.js
@@ -0,0 +1,15 @@
+function findLongestCommonSubstring(str1, str2){
+    let stringA = str1.split('');
+    let stringB = str2.split('');
+     let commonStringLength = 0;
+   
+    for(let i = 0; i < stringA.length; i++){
+            if(stringA[i] == stringB[i]){
+              commonStringLength ++;
+            }else if(stringA[i] != stringB[i]){
+                return commonStringLength;
+            }
+    }
+ }
+ 
+ console.log(findLongestCommonSubstring('Minnesota', 'Minneaopolis'));
