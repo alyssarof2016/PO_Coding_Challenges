faad5b20d519ce096aa268581abc17ec53088924
alyssarof2016 <68204695+alyssarof2016@users.noreply.github.com>
2021-08-02 15:01:40 -0400

    logical reasoning question

---
+++ Week 1/LogicalReasoning.js
@@ -0,0 +1,26 @@
+/*Logical Reasoning:
+3) This question is based on a certain code language. Understand the logic in the coding and answer the following question.
+
+If PROCESSOR is coded as D4F3C5C1E1S1S1E3C6, what is the code for QUADRANT?*/
+
+/*Alphabet
+A - 1   Q - 17
+B - 2   R - 18
+C - 3   S - 19
+D - 4   T - 20
+E - 5   U - 21
+F - 6   V - 22
+G - 7   W - 23
+H - 8   X - 24
+I - 9   Y - 25
+J - 10  Z - 26
+K - 11
+L - 12
+M - 13
+N - 14
+O - 15
+P - 16 */
+
+Q = Q1, U = C7, A = A1, D = B2, R = F3, A = A1, N = B7, T = E4
+
+Q1C7A1B2F3A1B7E4
